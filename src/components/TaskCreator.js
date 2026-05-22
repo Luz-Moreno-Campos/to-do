24a98eb057fd08418daede197431283ee37c3e06
Luz Moreno Campos <luzdirection@gmail.com>
2026-05-22 14:34:41 -0500

    created useState hook to display error message and styled error message

--- src/components/TaskCreator.js
+++ src/components/TaskCreator.js
@@ -4,6 +4,8 @@ import taskManagerLogo from "../assets/media/task-manager-logo.png";
 function TaskCreator(props) {
 
   const [title, setTitle] = useState("");
+  const [error, setError] = useState("");
+
 
   useEffect(() => {
     if (props.editingTask && props.editingTask.title !== title) {
@@ -17,33 +19,41 @@ function TaskCreator(props) {
 
   const handleSubmit = () => {
 
-    if (!title) return null;
-
+    if (!title) {
+      setError("** You can't create or save an empty task");
+      return;
+    }
     if (isEditing) {
-
       props.onEditTask(props.editingTask.id, title)
-      setTitle("");
-
+      
     } else {
-
-      props.onCreateTask(title);
-      setTitle("");
+      props.onCreateTask(title);  
 
     }
-
+       setTitle("");
+       setError("")
   };
 
   return (
     <section className="task-creator">
       <img src={taskManagerLogo} className="logo" alt="Task Manager Logo" />
       <p className="slogan">Your tasks organized, your day simplified.</p>
-       <h2>Create a new task</h2>
+      <h2>Create a new task</h2>
       <div className="input-wrapper">
-        <input type="text" placeholder="Enter your task here" value={title} maxLength={50} onChange={(e) => setTitle(e.target.value)} />
+        <input
+          type="text" placeholder="Enter your task here" value={title} maxLength={50}
+          onChange={(e) => {
+            setTitle(e.target.value);
+            setError("");
+          }}
+        />
         <button className="creator-btn" onClick={handleSubmit}>{isEditing ? "Save" : "Create"}</button>
+      </div>
+      <div className="error-container">
+       {error && <p clasName="error-message" className="error-message">{error}</p>}
       </div>
     </section >
   );
-}
 
+}
 export default TaskCreator;
